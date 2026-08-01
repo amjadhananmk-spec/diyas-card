940fb97a1a0334a490b6bd55280c1b14002c5556
amjadhananmk-spec <amjadhananmk@gmail.com>
2026-08-01 19:31:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function playMusic() {
     document.getElementById("bgMusic").play();
 }
 function surprise() {
-    alert("WILL YOU MARRY ME CHAKKARE I love you forever  i miss you take care be happy ponney  pic ayakkan pic ayakan marakanda❤️🥰");
+    alert("WILL YOU MARRY ME CHAKKARE I love you forever  i miss you take care be happy ponney pic ayakan marakanda  ❤️🥰 kathu kittiya muthannu thedi kittiya ponnane kattaduth oru mohasothanu");
 }
 
 document.getElementById("countdown").innerHTML = "🕚 11:11";
