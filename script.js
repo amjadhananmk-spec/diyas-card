f18ee4c9fcaf4269030b007c4a1eda42f7333ae1
amjadhananmk-spec <amjadhananmk@gmail.com>
2026-08-01 16:10:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function playMusic() {
+    document.getElementById("bgMusic").play();
+}
+function surprise() {
+    alert("WILL YOU MARRY ME CHAKKARE I love you forever  i miss you take care be happy ponney  pic ayakkan pic ayakan marakanda❤️🥰");
+}
+
+document.getElementById("countdown").innerHTML = "🕚 11:11";
