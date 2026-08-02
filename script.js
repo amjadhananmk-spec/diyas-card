c0cf5ed4a43bad1b45e7f5fd0741b0ed52ca07d7
amjadhananmk-spec <amjadhananmk@gmail.com>
2026-08-03 04:00:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,57 @@ function playMusic() {
     document.getElementById("bgMusic").play();
 }
 function surprise() {
-    alert("WILL YOU MARRY ME CHAKKARE I love you forever  i miss you take care be happy ponney pic ayakan marakanda  ❤️🥰 kathu kittiya muthannu thedi kittiya ponnane kattaduth oru mohasothanu");
+    alert("തു പാസ് ഹോ മേമേ
+പാസ് ഹോ മേമേ
+മേം കൊകി യീൻസ് കോ ലൈസേ
+തു പാസ് ഹോ മേമേ
+പാസ് ഹോ മേമേ
+മേം കൊകി യീൻസ് കോ ലൈസേ
+
+ഹായ് മേർ മി ജാവും
+ജോ തുജ്കോ ന പാവും
+ബാതോനാ മേൻ തേർ
+രാതോനിലാവും
+ഹോന്തോൻ സെ പലം ഹോഠാ
+ഹോ മേ തേരാ ഹായ
+തുജ്കോ ഹി പാവും
+മേൻ തുജ്കോ പാവും
+
+സായാ തു തൊ ബദൽ ഹമി ഹോ...
+മോസം സാ സാ തേരാ ഹുവാ ഹോ..
+സായാ തു തൊ ബദൽ ഹമി ഹോ..
+മോസം സാ സാ തേരാ ഹുവാ ഹോ   
+En kannukulla kathaadha
+
+Nenjukulla nachaadha
+
+Solle podhum
+
+Un solle podhum
+
+En anbamothom pichane
+
+Kovam vechu thechaane
+
+Innum venum
+
+Oh innum venum
+
+Vittu pona vaasalile
+
+Vaazhka theera kaathirukken
+
+Kaathilulla kaadhalellam
+
+Kooti kaathu thaarenen naan
+
+Oda oda naan mela saanjika
+
+Theera theera un thollaiya
+
+Eeram eeram kann oram
+
+Medhuvaa kothume....");
 }
 
 document.getElementById("countdown").innerHTML = "🕚 11:11";
